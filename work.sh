86de22031626bf1d8badfe82fc5bc01b7d87601b
API Maker <apimaker.dev@gmail.com>
2024-02-27 09:20:58 +0530

    1.3.3 release

--- work.sh
+++ work.sh
@@ -3,13 +3,16 @@
 ### ✓ Environment variables
 ```sh
 export DOCKER_USERNAME=savainfosystems
-export AM_IMAGE_VERSION=1.3.2
+export AM_IMAGE_VERSION=1.3.3
 export AM_IMAGE_VERSION=latest
+export AM_IMAGE_VERSION=0.0.1-alpha
 ```
 
 
 ### 💠 Sample commands
 ```sh
+# Need to publish with version & latest tag
+cd src
 docker build . -t ${DOCKER_USERNAME}/api_maker_be_fe:$AM_IMAGE_VERSION --platform linux/amd64
 docker run ${DOCKER_USERNAME}/api_maker_be_fe:$AM_IMAGE_VERSION
 docker push ${DOCKER_USERNAME}/api_maker_be_fe:$AM_IMAGE_VERSION
